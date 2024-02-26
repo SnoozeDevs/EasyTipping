71179572f7a74153a8c86de29bebb2c34bb06933
Connor Rendoth <connorrendoth11@gmail.com>
2024-02-26 14:54:42 +1100

    revert tab one name

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Login",
+          title: "Tab One",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -50,7 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: "Standings",
+          title: "Ladder",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />

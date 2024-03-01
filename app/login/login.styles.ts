065a4e479f023a7468296f8bc556fcf928eb0a2b
thomasrichmond <thomas.richmond8@gmail.com>
2024-03-02 00:18:16 +1100

    Adding styled component to project and implemented in login and signup pages

---
+++ app/login/login.styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native'
+
+export const Login = styled.View`
+
+    background-color: transparent;
+    flex: 1;
+    align-items: stretch;
+    justify-content: center;
+    align-content: center;
+    gap: 10px;
+    padding: 60px;
+`
+
+export const Link = styled.Text`
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    color: #000000;
+    text-decoration: underline;`

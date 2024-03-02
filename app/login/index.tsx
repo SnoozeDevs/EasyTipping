b627c8c4e0f4246820f06d3711a222d7715aa754
thomasrichmond <thomas.richmond8@gmail.com>
2024-03-02 15:09:17 +1100

    Adding error messages  and input boundaries to login screen

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -14,6 +14,14 @@ import {
   TextInput as PaperTextInput,
   Button as PaperButton,
 } from "react-native-paper";
+import {
+  EMAIL_ALREADY_IN_USE,
+  EMAIL_INVALID,
+  INCORRECT_PASSWORD,
+  NO_EMAIL_ENTERED,
+  NO_PASSWORD_ENTERED,
+  isEmailValid,
+} from "../utils/constants";
 
 export default function Login() {
   const searchParams = useLocalSearchParams();
@@ -22,14 +30,16 @@ export default function Login() {
   const [hasEnteredDetails, setHasEnteredDetails] = useState(false);
   const [emailHasError, setEmailHasError] = useState(false);
   const [passwordHasError, setPasswordHasError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [passwordSecured, setPasswordSecured] = useState(true);
+  const [isLoginLoading, setIsLoginLoading] = useState(false);
 
   useEffect(() => {
     if (searchParams.email) {
       setEmail(searchParams?.email as string);
     }
   }, [searchParams]);
-  // const [isEnabled, setIsEnabled] = useState(false); //todo toggle secureTextEntry prop on password entry
-  // const toggleSwitch = () => setIsEnabled(previousState => !previousState); //todo toggle secureTextEntry prop on password entry
 
   useEffect(() => {
     if (email && password) {
@@ -38,45 +48,61 @@ export default function Login() {
   }, [email, password]);
 
   const loginUser = () => {
-    //TODO Add regex to validate email before submission.
     let hasError = false;
-
     setEmailHasError(false);
     setPasswordHasError(false);
+    setEmailErrorMessage("");
+    setPasswordErrorMessage("");
 
     if (email === "") {
       hasError = true;
       setEmailHasError(true);
+      setEmailErrorMessage(NO_EMAIL_ENTERED);
+    } else if (!isEmailValid(email)) {
+      hasError = true;
+      setEmailHasError(true);
+      setEmailErrorMessage(EMAIL_INVALID);
     }
 
     if (password === "") {
       hasError = true;
       setPasswordHasError(true);
+      setPasswordErrorMessage(NO_PASSWORD_ENTERED);
     }
 
     if (hasError) {
       return;
     }
 
+    setIsLoginLoading(true);
+
     auth()
       .signInWithEmailAndPassword(email, password)
       .then((res) => {
         console.log("User account logged in!", res);
+        setIsLoginLoading(false);
         router.navigate("/dashboard");
       })
       .catch((error) => {
+        //TODO add too many requests error pathway
         if (error.code === "auth/email-already-in-use") {
           console.log("That email address is already in use!");
           setEmailHasError(true);
+          setEmailErrorMessage(EMAIL_ALREADY_IN_USE);
+          setIsLoginLoading(false);
         }
         if (error.code === "auth/invalid-email") {
           setEmailHasError(true);
+          setEmailErrorMessage(EMAIL_INVALID);
+          setIsLoginLoading(false);
           console.log("That email address is invalid!");
         }
 
         if (error.code === "auth/invalid-credential") {
           setPasswordHasError(true);
-          console.log("That email address is invalid!");
+          setPasswordErrorMessage(INCORRECT_PASSWORD);
+          setIsLoginLoading(false);
+          console.log("Password invalid!");
         }
         console.error(error);
       });
@@ -84,31 +110,54 @@ export default function Login() {
 
   return (
     <S.Login>
-      <PaperTextInput
-        autoCapitalize="none"
-        mode="outlined"
-        label="Email"
-        onChangeText={(event: any) => {
-          setEmail(event);
-        }}
-        error={emailHasError}
-        value={email ?? ""}
-        placeholder="Enter email address"
-      />
-      <PaperTextInput
-        autoCapitalize="none"
-        mode="outlined"
-        secureTextEntry //todo toggle prop (boolean)
-        right={<PaperTextInput.Icon icon="eye" />} //todo create onPress to toggle secureTextEntry prop
-        label="Password"
-        error={passwordHasError}
-        onChangeText={(event: any) => {
-          setPassword(event);
-        }}
-        value={password ?? ""}
-        placeholder="Enter password"
-      />
-      <PaperButton icon="" mode="contained" onPress={loginUser}>
+      <S.InputContainer>
+        <PaperTextInput
+          style={{ width: "100%" }}
+          autoCapitalize="none"
+          mode="outlined"
+          label="Email"
+          onChangeText={(event: any) => {
+            setEmail(event);
+          }}
+          error={emailHasError}
+          value={email ?? ""}
+          placeholder="Enter email address"
+        />
+        {emailHasError && <S.Error>{emailErrorMessage}</S.Error>}
+      </S.InputContainer>
+      <S.InputContainer>
+        <PaperTextInput
+          style={{ width: "100%" }}
+          autoCapitalize="none"
+          mode="outlined"
+          secureTextEntry={passwordSecured} //todo toggle prop (boolean)
+          right={
+            <PaperTextInput.Icon
+              onPressIn={() => {
+                setPasswordSecured(false);
+              }}
+              onPressOut={() => {
+                setPasswordSecured(true);
+              }}
+              icon="eye"
+            />
+          }
+          label="Password"
+          error={passwordHasError}
+          onChangeText={(event: any) => {
+            setPassword(event);
+          }}
+          value={password ?? ""}
+          placeholder="Enter password"
+        />
+        {passwordHasError && <S.Error>{passwordErrorMessage}</S.Error>}
+      </S.InputContainer>
+      <PaperButton
+        style={{ width: "100%" }}
+        icon=""
+        mode="contained"
+        onPress={loginUser}
+        loading={isLoginLoading}>
         Login
       </PaperButton>
 

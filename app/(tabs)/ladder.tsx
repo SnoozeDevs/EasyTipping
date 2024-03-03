6bdc2e2de9f7ce10acca065c2aa53841c38f0dd7
thomasrichmond <thomas.richmond8@gmail.com>
2024-03-03 16:45:33 +1100

    updating user fetch function calls and firestore db connections

--- app/(tabs)/ladder.tsx
+++ app/(tabs)/ladder.tsx
@@ -2,44 +2,80 @@ import { StyleSheet, FlatList, Button } from "react-native";
 import axios from "axios";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import auth from "@react-native-firebase/auth";
 import { router } from "expo-router";
+import { useFocusEffect, useIsFocused } from "@react-navigation/native";
+import firestore from "@react-native-firebase/firestore";
 
 export default function Dashboard() {
   const [teamData, setTeamData] = useState([]);
   const [isTeamDataLoaded, setIsTeamDataLoaded] = useState(false);
+  const [currentUser, setCurrentUser] = useState(auth().currentUser);
+  const isFocused = useIsFocused();
+  const requestController = new AbortController();
+
+  const getTeams = async () => {
+    const teams = await firestore().collection("standings").get();
+    return teams;
+  };
+
+  console.log(getTeams());
+
+  //TODO ||| Create boolean to return loader when user data is not ready to be displayed,
+  //TODO ||| this can be broken up to loading states for different parts of the page, and will provide load states for when data is not
+  //TODO ||| fetched from the DB or from firebase (eg to display their name).
+  useEffect(() => {
+    setCurrentUser(auth().currentUser);
+  }, [isFocused]);
 
   const teamDataFromDB = async () => {
     let teamData: any = [];
 
-    await axios.get("https://getteams-5yzqky2riq-uc.a.run.app").then((res) => {
-      res.data.map((team: any) => {
-        teamData.push(team.team);
-      });
-    });
+    await axios
+      .get("https://getteams-5yzqky2riq-uc.a.run.app", {
+        signal: requestController.signal,
+      })
+      .then((res) => {
+        res.data.map((team: any) => {
+          teamData.push(team.team);
+        });
 
-    setTeamData(teamData);
-    setIsTeamDataLoaded(true);
+        setTeamData(teamData);
+        setIsTeamDataLoaded(true);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
   };
+
+  //* Cancel DB calls if page goes out of focus.
+  useFocusEffect(
+    useCallback(() => {
+      if (!isTeamDataLoaded) {
+        teamDataFromDB();
+      }
+
+      return () => {
+        requestController.abort();
+      };
+    }, [teamDataFromDB])
+  );
 
   const signOutUser = () => {
     auth()
       .signOut()
       .then(() => {
-        router.navigate("signup");
+        setCurrentUser(null);
+        router.navigate("login");
         console.log("user signed out");
       });
   };
 
-  useEffect(() => {
-    teamDataFromDB();
-  }, []);
-
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Dashboard</Text>
-      <Text>Current user: {auth().currentUser?.displayName}</Text>
+      <Text>Current user: {currentUser?.displayName}</Text>
       <Button title="Signout" onPress={signOutUser} />
       <View
         style={styles.separator}

72ac18cc18cc8c553545b818ce6e8727cd10aa85
thomasrichmond <thomas.richmond8@gmail.com>
2024-03-09 23:19:31 +1100

    Adding profile page to drawer and removing fixture page from tab layout

---
+++ app/(pageRoutes)/profile.tsx
@@ -0,0 +1,16 @@
+import { DrawerToggleButton } from "@react-navigation/drawer";
+import { View, Text } from "react-native";
+
+export default function Settings() {
+  return (
+    <View
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: 1000,
+      }}>
+      <Text>This is the profile page</Text>
+    </View>
+  );
+}

686daac5337f3db11c8a53e67ff4a0f0dedbd5c5
thomasrichmond <thomas.richmond8@gmail.com>
2024-03-11 17:08:32 +1100

    Refactoring custom button to use RNP button functionality

--- components/Button/Button.styles.ts
+++ components/Button/Button.styles.ts
@@ -1,17 +1,9 @@
 import styled from 'styled-components/native'
+import { Button as PaperButton } from 'react-native-paper'
 
-export const Button = styled.Pressable`
+export const Button = styled(PaperButton)`
 display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: center;
-padding: 6px;
-background-color: #39A0ED;
-border-radius: 24px;
 width: 80%;
-height: 40px;
-gap: 8px;
-box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
 `
 export const Text = styled.Text`
 font-size: 16px;
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,21 +1,41 @@
 import { IButtonProps } from "./Button.types";
 import * as S from "./Button.styles";
 import { Text } from "react-native";
+import React from "react";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { stdTheme } from "@/themes/stdTheme";
+import { drkTheme } from "@/themes/drkTheme";
 
 const Button = ({
   title,
   iconName,
   iconPosition = "right",
   onPress,
+  dark,
+  loading,
+  compact,
+  disabled,
+  theme = "standard",
+  mode = "contained",
+  fullWidth,
 }: IButtonProps) => {
+  //* For icon options, see: https://callstack.github.io/react-native-paper/docs/guides/icons/#1-an-icon-name
+
   return (
-    <S.Button onPress={onPress}>
-      {iconPosition === "left" && <MaterialIcons name={iconName} size={25} />}
-      <S.Text>{title}</S.Text>
-      {iconPosition === "right" && (
-        <MaterialIcons name={iconName} size={16} style={{ color: "#fff" }} />
-      )}
+    <S.Button
+      style={fullWidth ? { width: "100%" } : {}}
+      contentStyle={{
+        flexDirection: `${iconPosition === "right" ? "row-reverse" : "row"}`,
+      }}
+      icon={iconName}
+      mode={mode}
+      theme={theme === "standard" ? stdTheme : drkTheme}
+      onPress={onPress}
+      dark={dark}
+      loading={loading}
+      compact={compact}
+      disabled={disabled}>
+      {title}
     </S.Button>
   );
 };

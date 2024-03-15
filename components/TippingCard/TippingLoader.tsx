88af22eecc9bfb074fc2d3dcf79572adc4f044f0
tom-richmond <thomas.richmond8@gmail.com>
2024-03-15 22:58:11 +1100

    Changing loader colour to theme constants

--- components/TippingCard/TippingLoader.tsx
+++ components/TippingCard/TippingLoader.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ContentLoader, { Path } from "react-content-loader/native";
+import * as S from "./TippingCard.styles";
 
 import { Rect, Circle } from "react-native-svg";
+import { Platform } from "react-native";
 const TippingLoader = (props: any) => (
   <ContentLoader
     speed={2}
     width={400}
-    height={160}
-    // width={"100%"}
-    // height={"100%"}
+    height={150}
+    id={props.id}
+    style={{ flex: 1, zIndex: -1, left: 20 }}
     viewBox="0 0 400 160"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
+    // foregroundColor="#ecebeb"
+    foregroundColor="#39A0ED"
     {...props}>
-    <Rect x="121" y="170" rx="3" ry="3" width="380" height="6" />
     <Rect x="575" y="238" rx="0" ry="0" width="5" height="4" />
     <Rect x="572" y="188" rx="0" ry="0" width="28" height="55" />
     <Rect x="582" y="231" rx="0" ry="0" width="3" height="7" />

25d73bf21c09de89489eb544a5b741601f03b4a1
thomasrichmond <thomas.richmond8@gmail.com>
2024-05-08 00:01:45 +1000

    Adding last round to group card type

--- components/GroupCard/GroupCard.types.ts
+++ components/GroupCard/GroupCard.types.ts
@@ -4,4 +4,5 @@ export interface IGroupCardProps {
   userRank: number;
   groupLeague: string;
   roundForm?: Array<string> //todo convert this once we know what the object looks like
+  lastRound?: string;
 }

78cef3762ca8c4e041ccf0d8d0c89a3eaaf2d1be
thomasrichmond <thomas.richmond8@gmail.com>
2024-05-11 17:34:54 +1000

    Chore: Updating component template config

--- components/_componentTemplate/ComponentTemplate.tsx
+++ components/_componentTemplate/ComponentTemplate.tsx
@@ -1,10 +1,12 @@
 import { IComponentTemplateProps } from "./ComponentTemplate.types";
+import * as S from "./ComponentTemplate.styles";
+import React from "react";
 
 const ComponentTemplate = ({}: IComponentTemplateProps) => {
   return (
-    <ComponentTemplate>
+    <S.ComponentTemplate>
       <></>
-    </ComponentTemplate>
+    </S.ComponentTemplate>
   );
 };
 

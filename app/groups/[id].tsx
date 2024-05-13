2abbff96d9a6cf75e72cfed27619ac0d3a977d5b
thomasrichmond <thomas.richmond8@gmail.com>
2024-05-13 23:13:20 +1000

    Fetch group data from global state to reduce repeat db fetches

--- app/groups/[id].tsx
+++ app/groups/[id].tsx
@@ -9,19 +9,17 @@ import {
   useGlobalContext,
 } from "@/utils/AppContext";
 import { getGroupData } from "@/utils/Groups/utils";
+import { isObjectEmpty } from "@/utils/Generic/utils";
 
 export default function GroupPage() {
   const { id, name, rank } = useLocalSearchParams();
-  const [groupData, setGroupData] = useState();
-
-  // const groupData = useMemo(() => getGroupData(id.toString()), [id]);
+  const groupId = id.toString() as keyof object;
+  const userRank = rank.toString();
+  const { groupSetter, groupValue }: GroupProviderType = useGlobalContext();
 
   useEffect(() => {
-    setTimeout(() => {
-      !groupData && getGroupData(id.toString(), setGroupData);
-    }, 3000);
+    getGroupData(groupId, groupSetter);
   }, [id]);
-  console.log("{id}", groupData);
 
   //* Need to get the users rank & collect all of the data that sits in the groups object
   //*   - Display a podium
@@ -30,15 +28,16 @@ export default function GroupPage() {
   //*   - Make table clickable which opens up a modal of the tippers form (and the teams they tipped)
   //!   - ! This means that when a user tips a game - the groups object needs to be updated and associated as well.
 
-  if (!groupData) {
+  if (isObjectEmpty(groupValue)) {
     return <Text>Loading group data...</Text>;
   }
 
   return (
     <GroupDetail
-      groupData={groupData}
+      groupData={groupValue[groupId]}
       groupId={id.toString()}
       groupName={name.toString()}
+      userRank={userRank}
     />
   );
 }
